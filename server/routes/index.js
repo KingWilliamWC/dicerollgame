7c71c153e545f768568990e91045f2c75286b80b
William Wolseley-Charles <williamwolseleycharles@gmail.com>
2022-01-17 21:56:43 +0000

    server config

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,5 +6,21 @@ router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, '../build/index.html'));
 });
 
+router.get('/login', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+router.get('/signup', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+router.get('/online', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
+router.get('/game', function(req, res, next) {
+  res.sendFile(path.join(__dirname, '../build/index.html'));
+});
+
 module.exports = router;
     
